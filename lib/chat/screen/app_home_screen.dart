016d894e0e41e41b64b5b1314378532618071bdc
Anil Bhattarai <bhattaraianil2015@gmail.com>
2026-01-19 19:51:48 +0545

    feat(UI): Navigation working sucessfully

--- lib/chat/screen/app_home_screen.dart
+++ lib/chat/screen/app_home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:chatapp_flutter/chat/screen/chat_list_screen.dart';
+import 'package:chatapp_flutter/chat/screen/user_list_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'profile_screen.dart';
 
 class MainHomeScreen extends StatefulWidget {
   const MainHomeScreen({super.key});
@@ -8,16 +12,31 @@ class MainHomeScreen extends StatefulWidget {
 }
 
 class _MainHomeScreenState extends State<MainHomeScreen> {
-  @override
+  int _currentIndex = 0;
+  // bool _hasInitialized = false;
+
+  //screen for each tab
+  //@override
+  final List<Widget> _screens = [
+    ChatListScreen(),
+    UserListScreen(),
+    ProfileScreen(),
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // body: const Center(),
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
         backgroundColor: Colors.grey.shade50,
         elevation: 0,
         unselectedFontSize: 14,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.chat), label: "chats"),
           BottomNavigationBarItem(icon: Icon(Icons.people), label: "Users"),
---
+++ lib/chat/screen/profile_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProfileScreen extends ConsumerStatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  ConsumerState<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends ConsumerState<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text("Profile", style: TextStyle(fontWeight: FontWeight.w600)),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+      ),
+    );
+  }
+}
