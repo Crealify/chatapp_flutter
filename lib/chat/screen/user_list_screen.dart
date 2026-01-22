0ce47d5c73ca7262f985f9c2909a33f0d002feaf
Anil Bhattarai <bhattaraianil2015@gmail.com>
2026-01-22 09:37:26 +0545

    logic: UserList refreshIndicator

--- lib/chat/screen/user_list_screen.dart
+++ lib/chat/screen/user_list_screen.dart
@@ -35,7 +35,58 @@ class _UserListScreenState extends ConsumerState<UserListScreen> {
         backgroundColor: Colors.white,
         centerTitle: true,
       ),
-      // body: RefreshIndicator(child: child, onRefresh: onRefresh),
+      body: RefreshIndicator(
+        //enable push to refresh
+        backgroundColor: Colors.white,
+        onRefresh: onRefresh,
+        child: users.when(
+          data: (userList) {
+            if (userList.isEmpty && searchQuery.isEmpty) {
+              return ListView(
+                children: [
+                  SizedBox(height: 200),
+                  Center(child: Text("No users found matching your search")),
+                ],
+              );
+            }
+            if (userList.isEmpty) {
+              return ListView(
+                children: [
+                  SizedBox(height: 200),
+                  Center(child: Text("No other user found")),
+                ],
+              );
+            }
+            return ListView.builder(
+              physics: AlwaysScrollableScrollPhysics(),
+              itemCount: userList.length,
+
+              itemBuilder: (context, index) {
+                final user = userList[index];
+                return Container();
+              },
+            );
+          },
+          error: (error, _) => ListView(
+            children: [
+              SizedBox(height: 200),
+              Center(
+                child: Column(
+                  children: [
+                    Text("Error: $error"),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () => ref.invalidate(usersProvider),
+                      child: Text("Retry"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          loading: () => Center(child: CircularProgressIndicator()),
+        ),
+      ),
     );
   }
 }
