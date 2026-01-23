52e8a5ec90d3434ede1b2b3bec5a413fbeb06d49
Anil Bhattarai <bhattaraianil2015@gmail.com>
2026-01-23 10:32:44 +0545

    Function(logic): ACCEPT REQUEST

--- lib/chat/screen/request_screen.dart
+++ lib/chat/screen/request_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp_flutter/chat/provider/provider.dart';
+import 'package:chatapp_flutter/core/utils/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -36,7 +37,64 @@ class _RequestScreenState extends ConsumerState<RequestScreen> {
       // =============== handle provider status(date, loading error)
       body: requests.when(
         //case 1 sucessfully loaded requests
-        data: (requestList) {},
+        data: (requestList) {
+          if (requestList.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.inbox, size: 16, color: Colors.white),
+                  SizedBox(height: 15),
+                  Text(
+                    "No pending request",
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                ],
+              ),
+            );
+          }
+          // if requests exists -> show List of requests
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final request = requestList[index];
+              return Card(
+                elevation: 0,
+                margin: EdgeInsets.all(8),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 28,
+                    backgroundImage: request.photoURL != null
+                        ? NetworkImage(request.photoURL!)
+                        : null,
+
+                    child: request.photoURL == null ? Icon(Icons.person) : null,
+                  ),
+                  title: Text(request.senderName),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      //ACCEPT REQUEST
+                      GestureDetector(
+                        onTap: () async {
+                          await ref
+                              .read(requestsProvider.notifier)
+                              .acceptRequest(request.id, request.senderId);
+                          if (context.mounted) {
+                            showAppSnackbar(
+                              context: context,
+                              type: SnackbarType.success,
+                              description: "Request accepted!",
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
 
         // case 2 error state
         error: (error, _) => Center(
@@ -52,7 +110,7 @@ class _RequestScreenState extends ConsumerState<RequestScreen> {
             ],
           ),
         ),
-        loading: ()=> Center(child: CircularProgressIndicator(),),
+        loading: () => Center(child: CircularProgressIndicator()),
       ),
     );
   }
