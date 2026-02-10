cd58d7772c7837f3506d001de2e2e7048b8e409b
Anil Bhattarai <bhattaraianil2015@gmail.com>
2026-02-10 09:02:48 +0545

    ui: data time format added and message display addec

--- lib/chat/screen/chat_screen/widgets/message_and_inage_display.dart
+++ lib/chat/screen/chat_screen/widgets/message_and_inage_display.dart
@@ -23,6 +23,60 @@ class MessageAndInageDisplay extends StatelessWidget {
         mainAxisAlignment: isMe
             ? MainAxisAlignment.end
             : MainAxisAlignment.start,
+        children: [
+          if (!isMe) ...[
+            CircleAvatar(
+              // radius: 60,
+              backgroundImage: widget.otherUser.photoURL != null
+                  ? NetworkImage(widget.otherUser.photoURL!)
+                  : null,
+              child: widget.otherUser.photoURL == null
+                  ? Text(
+                      widget.otherUser.name.isNotEmpty
+                          ? widget.otherUser.name[0].toLowerCase()
+                          : "U",
+                    )
+                  : null,
+            ),
+
+            SizedBox(width: 8),
+          ],
+          Flexible(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: message.type == "image" ? 8 : 10,
+                vertical: message.type == "image" ? 4 : 10,
+              ),
+              decoration: BoxDecoration(
+                color: isMe ? Colors.white : Colors.grey[300],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  // Image to display
+                  //Text message(for regular message or image capitons)
+                  if (message.message.isNotEmpty)
+                    Text(
+                      message.message,
+                      style: TextStyle(
+                        color: isMe ? Colors.white : Colors.black,
+                      ),
+                    ),
+
+                    SizedBox(height: 4,),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children:
+                       [
+                      ],
+                    )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
